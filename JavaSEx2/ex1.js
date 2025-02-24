28e2ce558eed53e8a5b6db923060151ce5ea98d9
Gabriella Prado <gabinathcarol@gmail.com>
2025-02-24 11:37:52 -0300

    Initial commit

---
+++ JavaSEx2/ex1.js
@@ -0,0 +1,19 @@
+function verificaVelocidade(){
+
+    let velocidade = window.document.getElementById('velocidade');
+    let valor = Number(velocidade.value);
+    let resultado = window.document.getElementById('resultado')
+       
+    if (valor > 40 && valor < 100){
+        console.log('Multado!')
+        resultado.innerHTML = '<strong>Multado</strong>';
+    }
+    else if (valor > 100){
+        console.log('Multado e veículo será apreendido!')
+        resultado.innerHTML = '<strong>Multado e veículo será apreendido!</strong>';
+    } else {
+        console.log('Dentro da velocidade permitida!')
+        resultado.innerHTML = '<strong>Dentro da velocidade permitida!</strong>';
+    }
+
+}
